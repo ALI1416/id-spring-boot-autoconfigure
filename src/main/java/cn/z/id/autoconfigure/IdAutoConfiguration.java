760784db5b025c3f108c872cd0f376d18d4dcc74
ALI <1416978277@qq.com>
2021-02-26 20:47:45 +0800

    v1.0.0

--- src/main/java/cn/z/id/autoconfigure/IdAutoConfiguration.java
+++ src/main/java/cn/z/id/autoconfigure/IdAutoConfiguration.java
@@ -1,12 +1,10 @@
-package cn.z.id.spring.boot.autoconfigure;
+package cn.z.id.autoconfigure;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import cn.z.id.Id;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import cn.z.id.Id;
+import javax.annotation.PostConstruct;
 
 /**
  * <h1>高性能Id生成器自动配置</h1>
@@ -38,17 +36,25 @@ public class IdAutoConfiguration {
     /**
      * IdProperties
      */
-    @Autowired
-    IdProperties idProperties;
+    private final IdProperties idProperties;
+
+    /**
+     * 构造函数
+     *
+     * @param idProperties IdProperties
+     */
+    public IdAutoConfiguration(IdProperties idProperties) {
+        this.idProperties = idProperties;
+    }
 
     /**
      * 初始化
      */
     @PostConstruct
-    private void init() {
+    public void addPageInterceptor() {
+        System.out.println(888);
         // 存在配置
-        if (!(idProperties.getMachineId() == null && idProperties.getMachineBits() == null
-                && idProperties.getSequenceBits() == null)) {
+        if (!(idProperties.getMachineId() == null && idProperties.getMachineBits() == null && idProperties.getSequenceBits() == null)) {
             // machineId为null去设置默认值
             if (idProperties.getMachineId() == null) {
                 idProperties.setMachineId(DEFAULT_MACHINE_ID);

cc668450f3ead101881d139a679fce0fc9c81ffd
ALI <1416978277@qq.com>
2021-02-27 13:01:37 +0800

    v1.0.2

--- src/main/java/cn/z/id/autoconfigure/IdAutoConfiguration.java
+++ src/main/java/cn/z/id/autoconfigure/IdAutoConfiguration.java
@@ -1,6 +1,8 @@
 package cn.z.id.autoconfigure;
 
 import cn.z.id.Id;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +23,10 @@ import javax.annotation.PostConstruct;
 public class IdAutoConfiguration {
 
     /**
+     * 日志实例
+     */
+    private final static Logger log = LoggerFactory.getLogger(IdAutoConfiguration.class);
+    /**
      * machineId默认值{@value}
      */
     private final static long DEFAULT_MACHINE_ID = 0L;
@@ -32,7 +38,6 @@ public class IdAutoConfiguration {
      * sequenceBits默认值{@value}
      */
     private final static long DEFAULT_SEQUENCE_BITS = 14L;
-
     /**
      * IdProperties
      */
@@ -51,22 +56,30 @@ public class IdAutoConfiguration {
      * 初始化
      */
     @PostConstruct
-    public void addPageInterceptor() {
+    public void init() {
         // 存在配置
         if (!(idProperties.getMachineId() == null && idProperties.getMachineBits() == null && idProperties.getSequenceBits() == null)) {
+            String msg = "读取到配置文件";
             // machineId为null去设置默认值
             if (idProperties.getMachineId() == null) {
                 idProperties.setMachineId(DEFAULT_MACHINE_ID);
+            } else {
+                msg += "，MACHINE_ID为" + idProperties.getMachineId();
             }
             // machineBits为null去设置默认值
             if (idProperties.getMachineBits() == null) {
                 idProperties.setMachineBits(DEFAULT_MACHINE_BITS);
+            } else {
+                msg += "，MACHINE_BITS为" + idProperties.getMachineBits();
             }
             // sequenceBits为null去设置默认值
             if (idProperties.getSequenceBits() == null) {
                 idProperties.setSequenceBits(DEFAULT_SEQUENCE_BITS);
+            } else {
+                msg += "，SEQUENCE_BITS为" + idProperties.getSequenceBits();
             }
-            // 初始化高性能Id生成器
+            log.info(msg);
+            // 初始化
             Id.init(idProperties.getMachineId(), idProperties.getMachineBits(), idProperties.getSequenceBits());
         }
     }
